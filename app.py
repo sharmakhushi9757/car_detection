5495a900355ad289e6c99c73ea426892d71a7e57
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-05-01 20:08:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ model = tf.keras.models.load_model(r"dmg_car-weights-CNN.h5")
 #
 def predict_image(img, model, threshold):
     # Load and preprocess the image
-    img = image.load_img(img, target_size=model.input_shape[1:4])
-    img_array = image.img_to_array(img)
+    img = load_img(img, target_size=model.input_shape[1:4])
+    img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     
     # Make predictions using the model
