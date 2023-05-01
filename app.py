688b80ee330c32863f10761a15aa98373e5b807a
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-05-01 19:50:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,26 +21,28 @@ explainer = lime_image.LimeImageExplainer()
 segmenter = SegmentationAlgorithm('slic', n_segments=50, compactness=10, sigma=1)
 #
 def predict_image(img, model, threshold):
-    # Load and preprocess the image
-    img = load_img(img, target_size=model.input_shape[1:4])
+    # Load image and preprocess it
+    img = load_img(image_path, model.input_shape[1:4])
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    
+    img_array /= 255.
+   
     # Make predictions using the model
-    prediction = model.predict(img_array)[0][0]
+    prediction = model.predict(img_array)
     
     # Check if the prediction is above the threshold
-    if prediction >= threshold:
+    '''if prediction >= threshold:
         result = 'good'
     else:
-        result = 'damaged'
-     # Generate an explanation of the prediction using LIME
-    expl = explainer.explain_instance(img_array[0], model.predict, top_labels=2, hide_color=0, num_samples=1000, segmentation_fn=segmenter)
-    explanation = expl.get_image_and_mask(expl.top_labels[0], positive_only=True, num_features=10, hide_rest=True)[0]
+        result = 'damaged''''
+    # Generate LIME explanation
+    explanation = explainer.explain_instance(img_array[0], model.predict, segmentation_fn=segmenter)
+    top_labels = prediction.argsort()[0][-3:]
+    explanation_html = explanation.as_html(top_labels=top_labels)
     
-    
-    # Return the result
-    return result,explanation
+    # Return prediction and explanation as a dictionary
+    return {'prediction': prediction[0], 'explanation_html': explanation_html}
+
 
 
 
@@ -52,7 +54,26 @@ st.sidebar.title('Settings')
 threshold = st.sidebar.slider('Threshold', 0.0, 1.0, 0.5, 0.01)
 def main():
   uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
-  if uploaded_file is not None:
+  #
+  if st.button('Predict'):
+        # Check if user has uploaded a file
+        if uploaded_file is not None:
+            # Make prediction and generate explanation using LIME
+            result = predict_image(model, uploaded_file, threshold)
+            prediction = result['prediction']
+            explanation_html = result['explanation_html']
+
+            # Display prediction and explanation
+            if prediction[0] >= threshold:
+                st.success('The car is undamaged with a probability of {}%'.format(round(prediction[0]*100, 2)))
+            else:
+                st.error('The car is damaged with a probability of {}%'.format(round(prediction[1]*100, 2)))
+            st.markdown('## Explanation')
+            st.write(explanation_html, unsafe_allow_html=True)
+        else:
+            st.warning('Please upload an image.')
+  #
+  '''if uploaded_file is not None:
         st.image(uploaded_file, caption='Input image', use_column_width=True)
         if st.button('Predict'):
             progress_bar = st.progress(0)
@@ -60,7 +81,7 @@ def main():
             progress_bar.progress(100)
             st.write('Prediction:', result)
             st.write('Explanation:')
-            st.image(explanation, caption='Explanation', use_column_width=True,clamp=True, channels='BGR')
+            st.image(explanation, caption='Explanation', use_column_width=True,clamp=True, channels='BGR')'''
 
 if __name__ == "__main__":
     main()
