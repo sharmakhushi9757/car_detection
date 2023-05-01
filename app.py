3c5d70d2b895e064f7c650dff859a2c1e469722f
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-05-01 17:45:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+# Predicting a new image using our CNN model
+import numpy as np
+from keras.preprocessing import image
+import tensorflow as tf
+import cv2
+import os
+from mtcnn_cv2 import MTCNN
+from tensorflow.keras.preprocessing.image import load_img , img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import streamlit as st
+
+
+model = tf.keras.models.load_model(r"dmg_car-weights-CNN.h5")
+
+#
+import streamlit as st
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+def predict_image(file_path, model, threshold):
+    # Load and preprocess the image
+    img = image.load_img(file_path, target_size=model.input_shape[1:4])
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    # Make predictions using the model
+    prediction = model.predict(img_array)[0][0]
+    
+    # Check if the prediction is above the threshold
+    if prediction >= threshold:
+        result = 'good'
+    else:
+        result = 'damaged'
+    
+    # Return the result
+    return result
+
+
+
+#
+
+def main():
+  st.title("Car Detection")
+
+  uploaded_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+    
+  if uploaded_file is not None:
+     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+     image = cv2.imdecode(file_bytes, 1)
+     # Call the function and display the result in Streamlit
+     result=predict_image(image, model,threshold)
+     st.image(image, caption='Input image', use_column_width=True)
+     st.write('Prediction:', result)
+     
+     
+     
+
+if __name__ == "__main__":
+    main()
