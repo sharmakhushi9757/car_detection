f33be9273e5a281480ce14b2460785abf9d5f0e3
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-05-06 14:33:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,34 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import streamlit as st
 
 
-model = tf.keras.models.load_model(r"damagemodel.h5")
+model_damage = tf.keras.models.load_model(r"damagemodel.h5")
+model_location= tf.keras.models.load_model(r"locationmodel.h5")
+model_serverity= tf.keras.models.load_model(r"serverity_model.h5")
 #
+def predict_image(img, model, threshold):
+    # Load and preprocess the image
+    img = load_img(img, target_size=model.input_shape[1:4])
+    img_array = img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    # Make predictions using the model
+    prediction = model.predict(img_array)
+    return prediction
 
-def pipe2(image_path, model):
+def pipe31(image_path, model):
     urllib.request.urlretrieve(image_path, 'save.jpg')
     img = load_img('save.jpg', target_size=(256,256))
     x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)/255
+    x = x.reshape((1,)+x.shape)/255
     pred = model.predict(x)
-    print("Validating that damage exists....")
-    print(pred)
-    if(pred[0][0]<=0.5):
-        st.write("Validation complete - proceed to location and severity determination")
-    else:
-        st.write("Are you sure that your car is damaged? Please submit another picture of the damage.")
-        st.write("Hint: Try zooming in/out, using a different angle or different lighting")    
+    pred_labels = np.argmax(pred, axis=1)
+    d = {0:'Front', 1:'Rear', 2:'Side'}
+    for key in d.keys():
+        if pred_labels[0] == key:
+            print("Validating location of damage....Result:",d[key])
+    print("Severity assessment complete.")
 
-def predict_image(img, model, threshold):
+def predictimage(img, model, threshold):
     # Load and preprocess the image
     img = load_img(img, target_size=model.input_shape[1:4])
     img_array = img_to_array(img)
@@ -53,13 +63,22 @@ def main():
   uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
   if st.button('Predict'):
         progress_bar = st.progress(0)
-        result=predict_image(uploaded_file, model, threshold)
+        result=predict_image(uploaded_file, model_damage, threshold)
         progress_bar.progress(100)
         if(result[0][0]>=0.5):
             st.write("Validation complete - proceed to location and severity determination")
+            result1=predictimage(uploaded_file, model_location, threshold)
+            pred_labels = np.argmax(pred, axis=1)
+            d = {0:'Front', 1:'Rear', 2:'Side'}
+            for key in d.keys():
+                if pred_labels[0] == key:
+                    st.write("Validating location of damage....Result:",d[key])
+            st.write("Severity assessment complete.")
         else:
             st.write("Are you sure that your car is damaged? Please submit another picture of the damage.")
-            st.write("Hint: Try zooming in/out, using a different angle or different lighting")   
+            st.write("Hint: Try zooming in/out, using a different angle or different lighting")
+        
+
  
   else:
     st.warning('Please upload an image.')
