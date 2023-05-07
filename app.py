631ea7fd91cf39716a52703556f27920a460fc5b
sharmakhushi9757 <66171879+sharmakhushi9757@users.noreply.github.com>
2023-05-07 11:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import streamlit as st
 
 model = tf.keras.models.load_model(r"dmg_car-weights-CNN.h5")
 model_location= tf.keras.models.load_model(r"locationmodel.h5")
-model_severity= tf.keras.models.load_model(r"severity_model.h5")
+model_severity= tf.keras.models.load_model(r"serverity_model.h5")
 
 #
 
